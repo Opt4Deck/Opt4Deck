2b5c1bdc32bc5cd5ca34f48ecf72e7370fa03391
Giannis Serafeim <opt4deck@gmail.com>
2025-12-27 17:49:55 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from SIMPLEX.simplex import main as simplex
+from BFGS.bfgs import main as bfgs
+from GENETIC.genetic import main as genetic
+from ADJOINT.adjoint import main as adjoint
+
+__all__ = ["simplex", "bfgs", "genetic", "adjoint"]
+
